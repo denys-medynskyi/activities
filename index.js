f1d38c937acf908e45c897f5e08768b0e1328cd6
Denys Medynskyi <denys.medynskyi@gmail.com>
2016-08-25 21:57:30 +0300

    remove react-router from index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
 import { loadState, saveState } from './localStorage'
 
 import App from './components/App'
