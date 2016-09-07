9b7960a53c0525420fb33771c2e774e0baac3db0
Denys Medynskyi <denys.medynskyi@gmail.com>
2016-09-07 08:58:06 +0300

    cleanup App.js

--- components/App.js
+++ components/App.js
@@ -1,21 +1,5 @@
 import React from 'react'
-import PageHeader from './PageHeader'
-import VisibleActivitiesList from '../containers/VisibleActivitiesList'
 import { Link } from 'react-router'
-
-// <div className="row">
-//     <div class="col s12">
-//     <nav>
-//     <div class="nav-wrapper">
-//     <div class="col s12">
-//     <a href="#!" class="breadcrumb">First</a>
-//     <a href="#!" class="breadcrumb">Second</a>
-//     <a href="#!" class="breadcrumb">Third</a>
-//     </div>
-//     </div>
-//     </nav>
-//     </div>
-//     </div>
 const App = (props) => {
     return (
         <div>
